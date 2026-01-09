be5e033b6068c8b3085a9662d2cbb453fb635798
peppezzzz <marano2552006@gmail.com>
2026-01-09 17:09:48 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+  bankroll: 1000,
+  baseBet: 1,
+  betSpread: [
+    { count: 0, mult: 1 },
+    { count: 2, mult: 2 },
+    { count: 4, mult: 4 },
+    { count: 6, mult: 6 }
+  ],
+  insuranceCount: 3,
+  bustItCount: 4
+};
